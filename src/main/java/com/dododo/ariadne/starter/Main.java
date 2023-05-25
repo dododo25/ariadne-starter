9a0fdf2a1020aeb73216dd23f968aebe5241d8e6
dododo <terekhov160997@gmail.com>
2023-05-24 23:29:19 -0700

    Add Main.java as a starting point of flowchart build process execution

---
+++ src/main/java/com/dododo/ariadne/starter/Main.java
@@ -0,0 +1,113 @@
+package com.dododo.ariadne.starter;
+
+import com.dododo.ariadne.common.configuration.Configuration;
+import com.dododo.ariadne.common.job.AbstractJob;
+import com.dododo.ariadne.common.provider.FlowchartJobsProvider;
+import com.dododo.ariadne.core.model.State;
+import com.dododo.ariadne.starter.list.AddItemsOnlyList;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+import java.util.concurrent.atomic.AtomicReference;
+
+public class Main {
+
+    private static final Logger LOG = LoggerFactory.getLogger(Main.class);
+
+    private static final Map<Class<? extends FlowchartJobsProvider>, Collection<String>> PROVIDER_PROFILES
+            = new HashMap<>();
+
+    public static void main(String... args) {
+        try {
+            Configuration configuration = createConfiguration();
+
+            FlowchartJobsProvider inputProvider =
+                    prepareProvider(configuration.getInputProfile());
+            FlowchartJobsProvider outputProvider =
+                    prepareProvider(configuration.getOutputProfile());
+
+            inputProvider.setConfiguration(configuration);
+            outputProvider.setConfiguration(configuration);
+
+            run(configuration, inputProvider, outputProvider);
+        } catch (ReflectiveOperationException e) {
+            LOG.error(e.getMessage(), e);
+        }
+    }
+
+    private static void run(Configuration configuration, FlowchartJobsProvider... distributors) {
+        AtomicReference<State> ref = new AtomicReference<>();
+
+        List<AbstractJob> jobs = new AddItemsOnlyList<>();
+
+        Arrays.stream(distributors)
+                .forEach(provider -> provider.populateJobs(jobs));
+
+        for (AbstractJob job : jobs) {
+            job.setConfiguration(configuration);
+            job.setFlowchart(ref.get());
+
+            LOG.info("Running {}", job.getName());
+
+            job.run();
+            ref.set(job.getFlowchart());
+
+            LOG.info("{} ends successfully with code 0", job.getName());
+        }
+    }
+
+    private static Configuration createConfiguration() {
+        Properties properties = new Properties();
+
+        System.getProperties().stringPropertyNames()
+                .stream()
+                .filter(key -> key.startsWith("flowchart"))
+                .forEach(key -> properties.setProperty(key, System.getProperty(key)));
+
+        validateProperties(properties);
+        prepareProperties(properties);
+
+        return Configuration.create(properties);
+    }
+
+    private static void validateProperties(Properties properties) {
+        validateProperty(properties, "flowchart.input.profile");
+        validateProperty(properties, "flowchart.output.profile");
+
+        boolean b = properties.stringPropertyNames().stream().anyMatch(k -> k.startsWith("flowchart.input.file"));
+
+        if (!b) {
+            throw new IllegalArgumentException("Config file must contain at least one 'flowchart.input.file' property");
+        }
+    }
+
+    private static void validateProperty(Properties properties, String key) {
+        if (!properties.containsKey(key)) {
+            throw new IllegalArgumentException(String.format("Config file must contain '%s' property", key));
+        }
+    }
+
+    private static void prepareProperties(Properties properties) {
+        if (!properties.containsKey("flowchart.output.directory")) {
+            properties.setProperty("flowchart.output.directory", ".");
+        }
+    }
+
+    private static FlowchartJobsProvider prepareProvider(String value)
+            throws ReflectiveOperationException {
+        Class<? extends FlowchartJobsProvider> providerType = PROVIDER_PROFILES.entrySet()
+                .stream()
+                .filter(entry -> entry.getValue().contains(value))
+                .findAny()
+                .map(Map.Entry::getKey)
+                .orElseThrow(IllegalArgumentException::new);
+
+        return providerType.getDeclaredConstructor().newInstance();
+    }
+}
---
+++ src/main/java/com/dododo/ariadne/starter/list/AddItemsOnlyList.java
@@ -0,0 +1,136 @@
+package com.dododo.ariadne.starter.list;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
+public class AddItemsOnlyList<E> implements List<E> {
+
+    private final List<E> list;
+
+    public AddItemsOnlyList() {
+        this.list = new ArrayList<>();
+    }
+
+    private AddItemsOnlyList(List<E> list) {
+        this.list = new ArrayList<>(list);
+    }
+
+    @Override
+    public int size() {
+        return list.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return list.isEmpty();
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return list.contains(o);
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return list.iterator();
+    }
+
+    @Override
+    public Object[] toArray() {
+        return list.toArray();
+    }
+
+    @Override
+    public <T> T[] toArray(T[] a) {
+        return list.toArray(a);
+    }
+
+    @Override
+    public boolean add(E e) {
+        return list.add(e);
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    @SuppressWarnings("SlowListContainsAll")
+    public boolean containsAll(Collection<?> c) {
+        return list.containsAll(c);
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends E> c) {
+        return list.addAll(c);
+    }
+
+    @Override
+    public boolean addAll(int index, Collection<? extends E> c) {
+        return list.addAll(index, c);
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void clear() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public E get(int index) {
+        return list.get(index);
+    }
+
+    @Override
+    public E set(int index, E element) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void add(int index, E element) {
+        list.add(index, element);
+    }
+
+    @Override
+    public E remove(int index) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        return list.indexOf(o);
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        return list.lastIndexOf(o);
+    }
+
+    @Override
+    public ListIterator<E> listIterator() {
+        return list.listIterator();
+    }
+
+    @Override
+    public ListIterator<E> listIterator(int index) {
+        return list.listIterator(index);
+    }
+
+    @Override
+    public List<E> subList(int fromIndex, int toIndex) {
+        return new AddItemsOnlyList<>(list.subList(fromIndex, toIndex));
+    }
+}
