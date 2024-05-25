e7da6f2f6a13849ba0f0823af794005e24413942
dododo <terekhov160997@gmail.com>
2024-05-25 17:12:31 +0300

    Initial commit

--- src/main/java/com/dododo/ariadne/starter/Main.java
+++ src/main/java/com/dododo/ariadne/starter/Main.java
@@ -5,38 +5,18 @@ import com.dododo.ariadne.common.job.AbstractJob;
 import com.dododo.ariadne.common.provider.CommonFlowchartJobsProvider;
 import com.dododo.ariadne.common.provider.FlowchartJobsProvider;
 import com.dododo.ariadne.core.model.State;
-import com.dododo.ariadne.drawio.provider.DrawIoFlowchartJobsProvider;
-import com.dododo.ariadne.renpy.rpy.provider.RenPyFlowchartJobsProvider;
-import com.dododo.ariadne.renpy.unity.provider.UnityFlowchartJobsProvider;
 import com.dododo.ariadne.starter.list.AddItemsOnlyList;
-import com.dododo.ariadne.thread.provider.ThreadFlowchartJobsProvider;
-import com.dododo.ariadne.xml.provider.XmlFlowchartJobsProvider;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Properties;
 import java.util.concurrent.atomic.AtomicReference;
 
 public class Main {
 
     private static final Logger LOG = LoggerFactory.getLogger(Main.class);
-
-    private static final Map<Class<? extends FlowchartJobsProvider>, Collection<String>> PROVIDER_PROFILES
-            = new HashMap<>();
-
-    static {
-        PROVIDER_PROFILES.put(ThreadFlowchartJobsProvider.class, Collections.singleton("ariadne"));
-        PROVIDER_PROFILES.put(DrawIoFlowchartJobsProvider.class, Collections.singleton("drawio"));
-        PROVIDER_PROFILES.put(RenPyFlowchartJobsProvider.class, Collections.singleton("renpy"));
-        PROVIDER_PROFILES.put(UnityFlowchartJobsProvider.class, Collections.singleton("unity"));
-        PROVIDER_PROFILES.put(XmlFlowchartJobsProvider.class, Collections.singleton("xml"));
-    }
 
     public static void main(String... args) {
         try {
@@ -115,14 +95,34 @@ public class Main {
 
     private static FlowchartJobsProvider prepareProvider(String value)
             throws ReflectiveOperationException {
-        Class<? extends FlowchartJobsProvider> providerType = PROVIDER_PROFILES.entrySet()
-                .stream()
-                .filter(entry -> entry.getValue().contains(value))
-                .findAny()
-                .map(Map.Entry::getKey)
-                .orElseThrow(IllegalArgumentException::new);
+        Class<?> providerType;
+        ClassLoader loader = ClassLoader.getSystemClassLoader();
 
-        return providerType.getDeclaredConstructor().newInstance();
+        try {
+            switch (value) {
+                case "ariadne":
+                    providerType = loader.loadClass("com.dododo.ariadne.thread.provider.ThreadFlowchartJobsProvider");
+                    break;
+                case "drawio":
+                    providerType = loader.loadClass("com.dododo.ariadne.drawio.provider.DrawIoFlowchartJobsProvider");
+                    break;
+                case "renpy":
+                    providerType = loader.loadClass("com.dododo.ariadne.renpy.rpy.provider.RenPyFlowchartJobsProvider");
+                    break;
+                case "xml":
+                    providerType = loader.loadClass("com.dododo.ariadne.xml.provider.XmlFlowchartJobsProvider");
+                    break;
+                case "unity":
+                    providerType = loader.loadClass("com.dododo.ariadne.renpy.unity.provider.UnityFlowchartJobsProvider;");
+                    break;
+                default:
+                    throw new IllegalArgumentException(String.format("Can`t find provider class for %s", value));
+            }
+        } catch (ClassNotFoundException e) {
+            throw new IllegalArgumentException(String.format("Can`t find provider class for %s", value), e);
+        }
+
+        return (FlowchartJobsProvider) providerType.getDeclaredConstructor().newInstance();
     }
 
     private static FlowchartJobsProvider prepareInnerProvider() {
